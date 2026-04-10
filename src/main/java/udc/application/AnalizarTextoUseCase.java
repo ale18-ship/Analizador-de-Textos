42be5a729cade47765db99d8c78326d47374ee0f
ale18-ship <acerap@unicartagena.edu.co>
2026-04-10 10:51:50 -0500

    feat(application): caso de uso para analizar y ordenar palabras

---
+++ src/main/java/udc/application/AnalizarTextoUseCase.java
@@ -0,0 +1,22 @@
+package udc.application;
+
+import udc.domain.TextoAnalizador;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class AnalizarTextoUseCase {
+    private final TextoAnalizador analizador = new TextoAnalizador();
+
+    public List<Map.Entry<String, Integer>> ejecutar(String texto) {
+
+        Map<String, Integer> resultado = analizador.analizar(texto);
+
+        List<Map.Entry<String, Integer>> lista = new ArrayList<>(resultado.entrySet());
+
+        lista.sort((a, b) -> b.getValue().compareTo(a.getValue()));
+
+        return lista;
+    }
+}
